50544788f1281515b8934cd8251e4d1d004fc241
Julie <julie.larkin.814@gmail.com>
2026-08-15 12:44:58 -0500

    SQS infra deployed

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create the SQS queue
+    const taskQueue = new sqs.Queue(this, 'TaskQueue', {
+      queueName: 'task-queue',
+      visibilityTimeout: cdk.Duration.seconds(30),
+      retentionPeriod: cdk.Duration.days(4),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Output the queue URL
+    new cdk.CfnOutput(this, 'TaskQueueUrl', {
+      value: taskQueue.queueUrl,
+    });
   }
 }
