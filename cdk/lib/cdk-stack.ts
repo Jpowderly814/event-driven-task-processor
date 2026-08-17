25d4e3a987866089a0820faa1f166e02ec24e838
Julie <julie.larkin.814@gmail.com>
2026-08-16 19:28:14 -0500

    create dlt and cloudwatch alarm for dlq

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch'
 import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as path from 'path';
 
@@ -9,12 +10,33 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+      //Deadletter queue for taskQueue
+      const dlq = new sqs.Queue(this, "TaskDLQ", {
+        queueName: "task-dlq",
+        retentionPeriod: cdk.Duration.days(3),
+      });
+
     // Create the SQS queue
-    const taskQueue = new sqs.Queue(this, 'TaskQueue', {
-      queueName: 'task-queue',
+    const taskQueue = new sqs.Queue(this, "TaskQueue", {
+      queueName: "task-queue",
       visibilityTimeout: cdk.Duration.seconds(30),
       retentionPeriod: cdk.Duration.days(4),
+      deadLetterQueue: {
+        maxReceiveCount: 3,
+        queue: dlq,
+      },
+    })
+
+    // DLQ Alarm
+    new cloudwatch.Alarm(this, "DLQAlarm", {
+      alarmName: "task-dlq-has-messages",
+      metric: dlq.metricApproximateNumberOfMessagesVisible(),
+      threshold: 1,
+      evaluationPeriods: 1,
+      comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      alarmDescription: "Triggered when the DLQ has at least 1 message.",
     });
+    
 
     // Create Producer Lambda
     const producerLambda = new lambda.Function(this, 'ProducerLambda', {
