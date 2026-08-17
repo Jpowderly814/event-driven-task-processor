99998993bab7880a4197e91483f6cb5c2dc669a5
Julie <julie.larkin.814@gmail.com>
2026-08-16 20:02:40 -0500

    refactor of stack file

---
+++ cdk/lib/lambda.ts
@@ -0,0 +1,37 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+import { Construct } from 'constructs';
+import * as path from 'path';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export function createLambdas(scope: Construct, taskQueue: sqs.Queue) {
+    // Create Producer Lambda
+    const producerLambda = new lambda.Function(scope, "ProducerLambda", {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        handler: "index.handler",
+        code: lambda.Code.fromAsset(path.join(__dirname, '../../src/producer')),
+        environment: {
+            TASK_QUEUE_URL: taskQueue.queueUrl,
+        },
+    });
+    taskQueue.grantSendMessages(producerLambda);
+
+    //Create Consumer Lambda
+    const consumerLambda = new lambda.Function(scope, "ConsumerLambda", {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        handler: "index.handler",
+        code: lambda.Code.fromAsset(path.join(__dirname, '../../src/consumer')),
+    });
+    taskQueue.grantConsumeMessages(consumerLambda);
+
+    // Lambdas should define their own triggers.  This belongs in this file where we create/define lambdas
+    //connects the queue - makes consumer lambda automatically trigger when messages arrive
+    // with addEventSource becomes "event driven"
+    consumerLambda.addEventSource(
+        new lambdaEventSources.SqsEventSource(taskQueue, {
+            batchSize: 10, // number of messages per invocation
+        })
+    );
+
+    return { producerLambda, consumerLambda };
+}
